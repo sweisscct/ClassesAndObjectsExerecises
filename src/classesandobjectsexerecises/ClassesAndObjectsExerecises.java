f11fea1453226901fe1c9165a98abece4140bfc3
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-11-28 20:20:12 +0000

    Hello World!

--- src/classesandobjectsexerecises/ClassesAndObjectsExerecises.java
+++ src/classesandobjectsexerecises/ClassesAndObjectsExerecises.java
@@ -6,7 +6,7 @@ package classesandobjectsexerecises;
 
 /**
  *
- * @author celeb
+ * @author sam
  */
 public class ClassesAndObjectsExerecises {
 
@@ -14,7 +14,7 @@ public class ClassesAndObjectsExerecises {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
