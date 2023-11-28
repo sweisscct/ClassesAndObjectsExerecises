51ebcc3114ed42b8b6369b54e812a654e94015e8
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-11-28 21:16:38 +0000

    Removed string

--- src/classesandobjectsexerecises/Car.java
+++ src/classesandobjectsexerecises/Car.java
@@ -18,8 +18,5 @@ public class Car {
         System.out.println(String.format("The %s %s is driving at %f degrees" ,colour, make, direction ));
         System.out.printf("The %s %s is driving at %f degrees" ,colour, make, direction );
     }
-    
-    public void drive(String direction) {
-        System.out.println("The " + colour + " " + make + " is driving " + direction);
-    }
+
 }
