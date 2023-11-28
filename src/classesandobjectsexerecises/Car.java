fcffcfb2f946897c52760417220d79136b530237
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-11-28 20:42:06 +0000

    double drive method

--- src/classesandobjectsexerecises/Car.java
+++ src/classesandobjectsexerecises/Car.java
@@ -13,4 +13,8 @@ public class Car {
     String model;
     int numWheels;
     String colour;
+    
+    public void drive(double direction) {
+        System.out.println(String.format("The %s %s is driving at %f degrees" ,colour, make, direction ));
+    }
 }
