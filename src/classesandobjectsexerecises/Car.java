aff573733617178703a012b97bcedd424190035f
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-11-28 20:31:09 +0000

    Created Car Class and properties
    make, model, numWheels, colour

---
+++ src/classesandobjectsexerecises/Car.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjectsexerecises;
+
+/**
+ *
+ * @author celeb
+ */
+public class Car {
+    String make;
+    String model;
+    int numWheels;
+    String colour;
+}
